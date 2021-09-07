a92842f9b4abbfe0b5380b96cf200357751a5d29
Carl <9841187+carlashley@users.noreply.github.com>
2021-09-07 10:46:49 +1000

    Create plist.py

---
+++ src/mcxlib/plist.py
@@ -0,0 +1,31 @@
+import plistlib
+
+
+def print(data: dict):
+    """Print property list to stdout"""
+    result = plistlib.dumps(data, fmt=plistlib.FMT_XML).decode('utf-8').strip()
+
+    return result
+
+
+def read(f: str):
+    """Read property list"""
+    result: dict = dict()
+
+    with open(f, 'rb') as _f:
+        result = plistlib.load(_f)
+
+    return result
+
+
+def read_from_string(s: bytes):
+    """Read property list"""
+    result: dict = plistlib.loads(s)
+
+    return result
+
+
+def write(data: dict, f: str):
+    """Write property list"""
+    with open(f, 'wb') as _f:
+        plistlib.dump(data, _f)
