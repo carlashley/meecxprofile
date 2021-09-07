a3eb0f7919c4e254e5bfbd0b581ee8de2b0fdf79
Carl <9841187+carlashley@users.noreply.github.com>
2021-09-07 10:46:25 +1000

    Update payload.py

--- src/mcxlib/payload.py
+++ src/mcxlib/payload.py
@@ -1,58 +1,64 @@
 """Configuration Profile"""
-import plistlib
-
 from datetime import datetime
 from uuid import uuid4
 
+from . import plist
 
-class Payload:
+
+class Profile:
     """Class to create Configuration Profile"""
-    def __init__(self, identifier: str, organization: str, display_name: str,
-                 removal_allowed=False, description='MCX Custom Settings for:\n'):
+    def __init__(self, identifier: str = 'meecxprofile', organization: str = 'com.github.carlashley',
+                 display_name: str = 'meecxprofile', removal_allowed: bool = False,
+                 description: str = 'MCX Custom Settings for:\n', payload_version: int = 1,
+                 payload_type: str = 'Configuration', payload_scope: str = 'System'):
         # Private
+        self._payload_version = payload_version
         self._identifier = identifier
         self._organization = organization
         self._display_name = display_name
         self._removal_allowed = removal_allowed
         self._description = description
-        self._profile_uuid = self.make_uuid()
+        self._payload_type = payload_type
+        self._payload_scope = payload_scope
+        self._profile_uuid = self._make_uuid()
 
         # Public
-        self.data = {'PayloadVersion': 1,
+        self.data = {'PayloadVersion': self._payload_version,
                      'PayloadOrganization': self._organization,
                      'PayloadRemovalDisallowed': self._removal_allowed,
                      'PayloadUUID': self._profile_uuid,
-                     'PayloadType': 'Configuration',
-                     'PayloadScope': 'System',
+                     'PayloadType': self._payload_type,
+                     'PayloadScope': self._payload_scope,
                      'PayloadDescription': self._description,
                      'PayloadDisplayName': self._display_name,
                      'PayloadIdentifier': self._identifier,
                      'PayloadContent': list()}  # Sub payloads that are provided later
 
-    def make_uuid(self):
-        return str(uuid4())
+    def _make_uuid(self):
+        """UUID generator"""
+        return str(uuid4()).upper()
 
-    def add_payload(self, payload: dict):
+    def _add_payload(self, payload: dict):
         """Add payload to the profile within the PayloadContent list
 
         :param payload (dict): payload to contain within the profile PayloadContent value"""
-        _uuid = self.make_uuid()
-        domains = [f' - {key}' for key, _ in payload.items()]
+        _uuid = self._make_uuid()
+        domains = [f'  - {key}' for key, _ in payload.items()]
 
-        if not self.description:
-            self.data['PayloadDescription'] = '\n'.join(domains)
+        if self._description.startswith('MCX Custom Settings for:\n'):
+            self.data['PayloadDescription'] += '\n'.join(domains)
 
         _payload = dict()
         _payload['PayloadVersion'] = 1
         _payload['PayloadUUID'] = _uuid
         _payload['PayloadEnabled'] = True
         _payload['PayloadType'] = 'com.apple.ManagedClient.preferences'
-        _payload['PayloadIdentifier'] = f'MCXToProfile.{self._profile_uuid}.alacarte.customsettings.{_uuid}'
+        _payload['PayloadIdentifier'] = f'{self._identifier}.{self._profile_uuid}.alacarte.customsettings.{_uuid}'
         _payload['PayloadContent'] = payload
 
         self.data['PayloadContent'].append(_payload)
 
-    def add_payload_from_plist(self, payload: dict, domain: str, manage: str, by_host=False):
+    def add_payload_from_plist(self, payload: dict, domain: str, manage: str, by_host: bool = False, keys: list = list()):
         """Add plist dict contents"""
         valid_manage = ['Always', 'Once']
 
@@ -61,6 +67,13 @@ class Payload:
 
         state = 'Forced' if manage == 'Always' else 'Set-Once'
         domain = f'{domain}.ByHost' if by_host else domain
+
+        # Remove keys that aren't required
+        if keys:
+            for key, value in payload.copy().items():
+                if key not in keys:
+                    del payload[key]
+
         _payload = dict()
         _payload[domain] = dict()
         _payload[domain][state] = list()
@@ -71,13 +84,12 @@ class Payload:
         if manage == 'Once':
             _payload[domain][state][0]['mcx_data_timestamp'] = datetime.utcnow()
 
-        self.add_payload(payload=_payload)
+        self._add_payload(payload=_payload)
 
     def add_payload_from_mcx(self, payload: dict):
         """Add MCX data"""
-        self.add_payload(payload=payload)
+        self._add_payload(payload=payload)
 
-    def write(self, f):
+    def write(self, f: str):
         """Write payload"""
-        with open(f, 'wb') as _f:
-            plistlib.dump(self.data, _f)
+        plist.write(self.payload, f)
